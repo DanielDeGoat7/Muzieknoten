ee60def5d9afb5597603dd827138eab05e843a3d
Daniel <s1213686@student.windesheim.nl>
2026-05-02 22:37:35 +0200

    feat: toegevoegd dat de rollen worden aangemaakt

--- Program.cs
+++ Program.cs
@@ -55,5 +55,18 @@ app.MapControllerRoute(
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    string[] roles = { "Docent", "Leerling" };
+
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+        {
+            await roleManager.CreateAsync(new IdentityRole(role));
+        }
+    }
+}
 
 app.Run();
