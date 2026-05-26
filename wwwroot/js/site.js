e2a7b0a95609625ba6cc2cdfc45442bd4bb92e27
Daniel <s1213686@student.windesheim.nl>
2026-05-26 20:19:30 +0200

    fix: alert verwijderd omdat die al in verstuurScoreNaarServer zit

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,6 +3,19 @@
         const container = document.getElementById('app-container') || document.getElementById('account-app-container');
         if (!container) return;
 
+        if (scherm !== 'speel-treble' && scherm !== 'speel-bass' && scherm !== 'speel-beide') {
+            if (actieveOefening && actieveOefening.totaalVragen > 0) {
+                    window.verstuurScoreNaarServer(
+                        actieveOefening.goedeAntwoorden, 
+                        actieveOefening.totaalVragen, 
+                        actieveOefening.huidigeOefeningId
+                    );
+                actieveOefening = null;
+                } else if (actieveOefening) {
+                    actieveOefening = null;
+                }
+        }
+
         const render = (templateId, data = {}) => {
             const element = document.getElementById(templateId);
             if (!element) return;
@@ -500,23 +513,6 @@ window.speelOefening = function(id, naam, oefeningType) {
         actieveOefening.speelOefening(id, naam);
     }
 }
-
-window.resetOefening = function() {
-    if (actieveOefening) {
-        if (actieveOefening.totaalVragen > 0) {
-            alert("Je hebt " + actieveOefening.totaalVragen + " vragen beantwoord. De score is opgeslagen");
-            window.verstuurScoreNaarServer(
-                actieveOefening.goedeAntwoorden, 
-                actieveOefening.totaalVragen, 
-                actieveOefening.huidigeOefeningId
-            );
-        }
-
-        actieveOefening = null;
-    }
-
-}
-
 
 
 document.addEventListener("DOMContentLoaded", () => {
